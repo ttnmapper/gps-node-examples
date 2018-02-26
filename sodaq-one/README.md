235dc384cf815ba9cf2d02558a0bbbbc9344c596
JP Meijers <gitkraken@jpmeijers.com>
2018-02-26 14:27:46 +0200

    Add Sodaq One example

---
+++ sodaq-one/README.md
@@ -0,0 +1,3 @@
+## Prerequisites
+You need to have the [RN2483 library](https://github.com/jpmeijers/RN2483-Arduino-Library) installed.
+
---
+++ sodaq-one/decoder.js
@@ -0,0 +1,27 @@
+function Decoder(bytes, port) {
+  // Decode an uplink message from a buffer
+  // (array) of bytes to an object of fields.
+  var decoded = {};
+
+  // if (port === 1) decoded.led = bytes[0];
+  decoded.lat = ((bytes[0]<<16)>>>0) + ((bytes[1]<<8)>>>0) + bytes[2];
+  decoded.lat = (decoded.lat / 16777215.0 * 180) - 90;
+  
+  decoded.lon = ((bytes[3]<<16)>>>0) + ((bytes[4]<<8)>>>0) + bytes[5];
+  decoded.lon = (decoded.lon / 16777215.0 * 360) - 180;
+  
+  var altValue = ((bytes[6]<<8)>>>0) + bytes[7];
+  var sign = bytes[6] & (1 << 7);
+  if(sign)
+  {
+    decoded.alt = 0xFFFF0000 | altValue;
+  }
+  else
+  {
+    decoded.alt = altValue;
+  }
+  
+  decoded.hdop = bytes[8] / 10.0;
+
+  return decoded;
+}
